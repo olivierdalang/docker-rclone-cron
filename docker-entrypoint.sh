2bdb04d8e85708d8bd8cab6e8b755877b662dd8a
olivierdalang <olivier.dalang@gmail.com>
2019-06-07 11:23:21 +0200

    run the command once on startup and print debug command

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 set -e
 
+# constructing the command
+COMMAND="rclone copy -v \"$RCLONE_SOURCE_PATH\" \"$RCLONE_DESTINATION_PATH\""
+
+# debug statement
+echo "The command is : $COMMAND"
+
+# running the command once, so that the container stops if it is misconfigured
+eval "$COMMAND"
+
 # setting up cron
-echo "$RCLONE_CRON_SCHEDULE rclone copy -v $RCLONE_SOURCE_PATH $RCLONE_DESTINATION_PATH" > /etc/crontabs/root
+echo "$RCLONE_CRON_SCHEDULE $COMMAND" > /etc/crontabs/root
 
 # run the CMD
 exec "$@"
